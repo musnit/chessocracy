f7aafac6f17786ec6750dbf20f6f744c28df9d4d
Aidan Musnitzky <musnit@gmail.com>
2018-03-27 11:58:41 +0200

    yarn, openzepelin, truffle setup

---
+++ contracts/DemocraticPlayer.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.4.19;
+
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract DemocraticPlayer {
+
+  function DemocraticPlayer() public {
+  }
+
+  function flip(bool _guess) public returns (bool) {
+  }
+
+}
--- truffle.js
+++ truffle.js
@@ -1,4 +1,17 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var secrets = require("./secrets.js");
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(secrets.mnemonic, "https://ropsten.infura.io/" + secrets.infura_apikey),
+      network_id: 3,
+      gas: 2708980
+    }
+  }
 };
